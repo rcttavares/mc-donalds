e7397267939979116d7de4b71f42a4e29f495bc2
Rafael Tavares <rafael.t@compasso.com.br>
2025-02-27 15:44:08 -0300

    feat: add initial restaurant page and menu page

---
+++ src/data/get-restaurant-by-slug.ts
@@ -0,0 +1,11 @@
+import { db } from "@/lib/prisma";
+
+export const getRestaurantBySlug = async (slug: string) => {
+  const restaurant = await db.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  return restaurant;
+};
